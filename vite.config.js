512cd28f57e80611c1461edbc94c14c9ea274c0a
nghiant <ngotiennghia25073@gmail.com>
2024-05-26 12:59:47 +0700

    feat: update image

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
-
+import { singleFile } from 'vite-plugin-singlefile';
 export default {
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, 'components/vi'),
         }),
+        singleFile()
     ],
 };
